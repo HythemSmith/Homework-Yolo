605096e14dda6690f872b216a717c605659f259d
Hythem Smith <longxuyen512vt@gmail.com>
2026-03-15 21:06:28 +0700

    commit

--- train.py
+++ train.py
@@ -7,18 +7,14 @@ from dotenv import load_dotenv
 if __name__ == '__main__':
     
     # 1. TẢI DATASET TỪ ROBOFLOW
-    load_dotenv()
-    rf = Roboflow(api_key=os.getenv("API_KEY"))
-    project = rf.workspace("adip-xskmh").project("homework-c85ln")
-    version = project.version(1)
-    dataset = version.download("yolov8")
+    
 
     # 2. KHỞI TẠO MÔ HÌNH YOLOv8 SEGMENTATION
     model = YOLO('yolov8s.pt')
 
     # 3. TIẾN HÀNH HUẤN LUYỆN (TRAINING)
     results = model.train(
-        data=f"{dataset.location}/data.yaml", 
+        data=f"object_detection_yolo/data.yaml", 
         epochs=100,
         imgsz=640,
         batch=16,
